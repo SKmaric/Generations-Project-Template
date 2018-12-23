01d906d4f0d7262f83a57911eb0ad409f11df0e4
SKmaric <25527946+SKmaric@users.noreply.github.com>
2018-12-23 13:36:56 +1100

    Update build.bat
    Bugfixes

--- build.bat
+++ build.bat
@@ -14,21 +14,31 @@ robocopy %~dp0includedfiles %~dp0build /MIR
 rem // Move modfolders contents excluding non-unpacked archives to build folder
 robocopy %~dp0modfolders %~dp0build\mods\ /XD *.ar /MIR
 
+cd %CD%\modfolders\	
+
 for /D /r %%G in ("*.ar") do (
 	set curr=%%~G
-	set curr=!curr:%CD%\modfolders\=!
+	set curr=!curr:%CD%=!
 	echo !curr!
 	
 	rem // Pack folders into archives
-	ar0pack.exe %%~G
+	..\ar0pack.exe %%~G
 	
 	rem // Move to build folder
 	for /l %%x in (0, 1, 99) do (
 		rem // Check for and move split files
-		if exist %%~G.ar.0%%x (
-			move /Y %%~G.ar.0%%x %~dp0build\mods\!curr!.0%%x
+		if %%x LSS 10 (
+			if exist %%~G.ar.0%%x (
+				move /Y %%~G.ar.0%%x %~dp0build\mods\!curr!.0%%x
+			) else (
+				break
+			)
 		) else (
-			break
+			if exist %%~G.ar.%%x (
+				move /Y %%~G.ar.%%x %~dp0build\mods\!curr!.%%x
+			) else (
+				break
+			)
 		)
 	)
 	move /Y %%~G.arl %~dp0build\mods\!curr!l
